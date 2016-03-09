d856936a857b4254f9f783c51ca0dfe0149014a4
yondop <yondop@gmail.com>
2016-03-09 21:06:01 +0500

    lab2

---
+++ lab2/calc.sh
@@ -0,0 +1,26 @@
+a=$1; b=$2; c=$3
+
+if [ $# -ne 3 ]; then
+  echo Enter all parameters: a b c
+  exit
+fi
+echo $a'x'^2+$b'x'+$c=0
+
+d=$(( $b*$b-4*$a*$c ))
+echo 'D= '$d
+
+if [ $d -gt 0 ]; then
+  x1=$(echo "scale=3; 0.5*(-$b+sqrt($d))/$a" | bc)
+  x2=$(echo "scale=3; 0.5*(-$b-sqrt($d))/$a" | bc)
+  echo 'x1= '$x1
+  echo 'x2= '$x2
+elif [ $d = 0 ]; then
+  x12=$(echo "scale=3; 0.5*(-$b)/$a" | bc)
+  echo 'x12= '$x12
+else
+  xr=$(echo "scale=3; 0.5*(-$b)/$a" | bc)
+  xi=$(echo "scale=3; 0.5*sqrt($((-$d)))/$a" | bc)
+  echo $xi
+  echo 'x1= '$xr' + '$xi'i'
+  echo 'x2= '$xr' - '$xi'i'
+fi
